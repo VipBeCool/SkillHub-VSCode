7aef53dc2731a919ac670477c34e66112eae5b60
VipBeCool <vipbecool@gmail.com>
2026-09-02 16:37:56 +0800

    refactor: update skillhub icon and remove redundant database utility scripts

--- read_db2.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-import os
-
-db_path = os.path.expanduser('~/Library/Application Support/com.skillhub.desktop/skillhub.sqlite')
-conn = sqlite3.connect(db_path)
-c = conn.cursor()
-c.execute("SELECT id, path, source_type FROM source_directories")
-print("Dirs:", c.fetchall())
